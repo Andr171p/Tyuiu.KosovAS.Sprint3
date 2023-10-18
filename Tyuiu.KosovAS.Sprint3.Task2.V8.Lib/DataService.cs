af7a3cf97295c4678fedfc112545b038a7592d79
Tyuiu.KosovAS@outlook.com <andrey.kosov.05@inbox.ru>
2023-10-18 17:36:59 +0500

    Task2

---
+++ Tyuiu.KosovAS.Sprint3.Task2.V8.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KosovAS.Sprint3.Task2.V8.Lib
+{
+    public class DataService : ISprint3Task2V8
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            int i = 1;
+            double result = 1;
+            do
+            {
+                result *= 1 / (Math.Pow((Math.Cos(5) + 1), 2));
+                i++;
+            } while ((i < stopValue) && (startValue <= i));
+            return Math.Round(result, 3);
+        }
+    }
+}
---
+++ Tyuiu.KosovAS.Sprint3.Task2.V8.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KosovAS.Sprint3.Task2.V8.Lib;
+
+namespace Tyuiu.KosovAS.Sprint3.Task2.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 1;
+            int stopValue = 15;
+
+            Assert.AreEqual(0.001, ds.GetMultiplySeries(startValue, stopValue));
+        }
+    }
+}
