9eb7be45141f146729bd8022433a296a38807dc9
Tyuiu.KosovAS@outlook.com <andrey.kosov.05@inbox.ru>
2023-10-18 16:04:12 +0500

    Add project files.

---
+++ Tyuiu.KosovAS.Sprint3.Task0.V22.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KosovAS.Sprint3.Task0.V22.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KosovAS.Sprint3.Task0.V22.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("94fbea94-b73e-4fcf-9032-d192bf9d520b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
